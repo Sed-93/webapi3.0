3c994c32d7a6d4b52a8d8b0071af1ae7124c5478
Sed-93 <sedat-933@hotmail.se>
2024-02-09 09:44:58 +0100

    change

--- webapi3.0/Program.cs
+++ webapi3.0/Program.cs
@@ -14,7 +14,7 @@ var app = builder.Build();
 // Ensure the controller is created by the DI container
 var controller = app.Services.GetRequiredService<Controller>();
 
-app.MapGet("/", () => "Välkommen till vår webbplats! Du kan kryptera text genom att gå till /encrypt/{dinText}/{shift} och dekryptera text genom att gå till /decrypt/{dinText}/{shift}.");
+app.MapGet("/", () => "Välkommen till vår webbplats!\nFör att kryptera text gå till /encrypt/{dinText}/{shift}\nFör att avkryptera text gå till /decrypt/{dinText}/{shift}.");
 app.MapGet("/encrypt/{plainText}/{shift}", (string plainText, int shift) => controller.Encrypt(plainText, shift));
 app.MapGet("/decrypt/{cipherText}/{shift}", (string cipherText, int shift) => controller.Decrypt(cipherText, shift));
 
